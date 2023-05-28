1db6c22588877b2b539fa4ed241c11eff28eb48c
Gelo <120197506+acajegas62@users.noreply.github.com>
2023-05-28 22:57:15 +0800

    Update FeedbackLayout.java

--- src/FeedbackLayout.java
+++ src/FeedbackLayout.java
@@ -1,142 +1,117 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class FeedbackLayout extends JFrame {
 
-    private JTextField nameField;
-    private JTextField subjectField;
-    private JTextField quizNameField;
-    private JTextArea feedbackArea;
-
     public FeedbackLayout() {
-        // Existing code...
+        // create a frame with title "Feedback"
+        super("Feedback");
+
+        // create a panel with grid layout for the components
+        JPanel panel = new JPanel(new GridBagLayout());
+        panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        // create a GridBagConstraints instance
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(5, 0, 5, 0);
+
+        // create the "QuizUp" label
+        JLabel titleLabel = new JLabel("QuizUp");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+
+        // add the titleLabel to the panel using GridBagConstraints
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        panel.add(titleLabel, gbc);
 
         // create labels and text fields
         JLabel nameLabel = new JLabel("Name:");
         nameLabel.setFont(new Font("Arial", Font.BOLD, 12));
-        nameField = new JTextField(20);
+        JTextField nameField = new JTextField(20);
 
         JLabel subjectLabel = new JLabel("Subject:");
         subjectLabel.setFont(new Font("Arial", Font.BOLD, 12));
-        subjectField = new JTextField(20);
+        JTextField subjectField = new JTextField(20);
 
         JLabel quizNameLabel = new JLabel("Quiz Name:");
         quizNameLabel.setFont(new Font("Arial", Font.BOLD, 12));
-        quizNameField = new JTextField(20);
+        JTextField quizNameField = new JTextField(20);
 
         JLabel feedbackLabel = new JLabel("Feedback Message:");
         feedbackLabel.setFont(new Font("Arial", Font.BOLD, 12));
-        feedbackArea = new JTextArea(5, 20);
+        JTextArea feedbackArea = new JTextArea(5, 20);
 
-        // Existing code...
+        // add labels and text fields to the panel using GridBagConstraints
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        gbc.anchor = GridBagConstraints.WEST;
+        panel.add(nameLabel, gbc);
 
-        // create an update button
-        JButton updateButton = new JButton("Update");
-        updateButton.setBackground(Color.decode("#FF8C00"));
-        updateButton.setForeground(Color.WHITE);
-        updateButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // handle update button action
-                String name = nameField.getText();
-                String subject = subjectField.getText();
-                String quizName = quizNameField.getText();
-                String feedback = feedbackArea.getText();
+        gbc.gridy = 2;
+        panel.add(subjectLabel, gbc);
 
-                // perform update logic here
-                // For demonstration purposes, let's just display the updated feedback
-                String updatedFeedback = "Name: " + name + "\n"
-                        + "Subject: " + subject + "\n"
-                        + "Quiz Name: " + quizName + "\n"
-                        + "Feedback Message: " + feedback;
+        gbc.gridy = 3;
+        panel.add(quizNameLabel, gbc);
 
-                JOptionPane.showMessageDialog(FeedbackLayout.this, "Feedback updated:\n\n" + updatedFeedback);
-            }
+        gbc.gridy = 4;
+        panel.add(feedbackLabel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.insets = new Insets(5, 10, 5, 0); // Add left padding to text fields
+        panel.add(nameField, gbc);
+
+        gbc.gridy = 2;
+        panel.add(subjectField, gbc);
+
+        gbc.gridy = 3;
+        panel.add(quizNameField, gbc);
+
+        gbc.gridy = 4;
+        panel.add(new JScrollPane(feedbackArea), gbc);
+
+        // create a submit button
+        JButton submitButton = new JButton("Submit");
+        submitButton.setBackground(Color.decode("#DD4A48"));
+        submitButton.setForeground(Color.WHITE);
+        submitButton.addActionListener(e -> {
+            // handle submit button action
+            String name = nameField.getText();
+            String subject = subjectField.getText();
+            String quizName = quizNameField.getText();
+            String feedback = feedbackArea.getText();
+
+            // perform feedback submission logic here
+
+            // display a confirmation message
+            JOptionPane.showMessageDialog(this, "Thank you for your feedback!");
         });
 
-        // create a delete button
-        JButton deleteButton = new JButton("Delete");
-        deleteButton.setBackground(Color.decode("#DD4A48"));
-        deleteButton.setForeground(Color.WHITE);
-        deleteButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // handle delete button action
-                int choice = JOptionPane.showConfirmDialog(FeedbackLayout.this,
-                        "Are you sure you want to delete this feedback?", "Confirmation", JOptionPane.YES_NO_OPTION);
-                if (choice == JOptionPane.YES_OPTION) {
-                    // perform delete logic here
-                    clearFields();
-                    JOptionPane.showMessageDialog(FeedbackLayout.this, "Feedback deleted!");
-                }
-            }
-        });
-
-        // create a panel for the update and delete buttons
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.add(updateButton);
-        buttonPanel.add(deleteButton);
-
-        // add the button panel to the main panel
-        gbc.gridy = 6;
+        // add the submit button to the panel
+        gbc.gridx = 0;
+        gbc.gridy = 5;
         gbc.gridwidth = 2;
-        panel.add(buttonPanel, gbc);
+        gbc.anchor = GridBagConstraints.CENTER;
+        panel.add(submitButton, gbc);
 
-        // create a save button
-        JButton saveButton = new JButton("Save");
-        saveButton.setBackground(Color.decode("#008000"));
-        saveButton.setForeground(Color.WHITE);
-        saveButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // handle save button action
-                String name = nameField.getText();
-                String subject = subjectField.getText();
-                String quizName = quizNameField.getText();
-                String feedback = feedbackArea.getText();
+        // add the panel to the frame
+        add(panel);
 
-                // perform save logic here
-                saveFeedbackToFile(name, subject, quizName, feedback);
+        // set the size of the frame
+        setSize(1000, 600);
 
-                // display a confirmation message
-                JOptionPane.showMessageDialog(FeedbackLayout.this, "Feedback saved!");
-            }
-        });
+        // set the default close operation
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // add the save button to the panel
-        gbc.gridy = 7;
-        panel.add(saveButton, gbc);
+        // center the frame on the screen
+        setLocationRelativeTo(null);
+
+        // make the frame visible
+        setVisible(true);
     }
-
-    private void clearFields() {
-        nameField.setText("");
-        subjectField.setText("");
-        quizNameField.setText("");
-        feedbackArea.setText("");
-    }
-
-    private void saveFeedbackToFile(String name, String subject, String quizName, String feedback) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("feedback.txt", true))) {
-            writer.write("Name: " + name);
-            writer.newLine();
-            writer.write("Subject: " + subject);
-            writer.newLine();
-            writer.write("Quiz Name: " + quizName);
-            writer.newLine();
-            writer.write("Feedback Message: " + feedback);
-            writer.newLine();
-            writer.newLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Existing code...
 
     public static void main(String[] args) {
         // create an instance of the FeedbackLayout
